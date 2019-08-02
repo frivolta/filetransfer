b15c2e15556e662b42f3f2a7d1284c7e65156264
frivolta <rivoltafilippo@gmail.com>
2019-08-02 23:52:14 +0200

    Upload and validation

---
+++ app/src/layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Home from '../pages/Home'
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className={'app-layout'}>
+        <Home />
+      </div>
+    )
+  }
+}
+export default Layout;
